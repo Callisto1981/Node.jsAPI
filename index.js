c78e4f4c9b79f3dc2fbafa06c3afae952690863f
Wayne <wabbruscatoorr@gmail.com>
2021-03-02 20:40:59 -0500

    update file

---
+++ index.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express;
+
+const courses = [
+    { id: 1, name: 'Italian'},
+    {id: 1, name: 'German'},
+    {id: 1, name: 'Arabic'},
+]
